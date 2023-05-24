7c12e1318e07e05662b0fe82c5e9ea9454442dbf
dhutsj <shuangjie.tian@elsevier.com>
2023-05-24 11:22:19 +0800

    first commit

---
+++ data_generator.py
@@ -0,0 +1,28 @@
+import json
+import xmltodict
+import os
+
+# assign directory
+directory = '$PATH'
+i = 0
+# iterate over xml files in
+# that directory and transfer into json files
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        print(f)
+        i = i + 1
+        with open(f) as xml_file:
+            data_dict = xmltodict.parse(xml_file.read())
+            # xml_file.close()
+
+            # generate the object using json.dumps()
+            # corresponding to json data
+
+            json_data = json.dumps(data_dict)
+
+            # Write the json data to output
+            # json file
+            with open(f"{str(i)}.json", "w") as json_file:
+                json_file.write(json_data)
+                # json_file.close()
