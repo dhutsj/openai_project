3db7289f40db3251ac13f1b0a26e1ca27687c7cb
Chris Shang <c.shang@elsevier.com>
2023-05-25 11:01:37 +0800

    add try/catch

--- myjsontransfer.py
+++ myjsontransfer.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # assign directory
-directory = '$PATH'
+directory = './'
 i = 0
 # iterate over files in
 # that directory
@@ -14,16 +14,23 @@ for filename in os.listdir(directory):
         mf = open(f)
         data = json.load(mf)
 
-        if type(data['doc:document']['ja:article']['ja:head']['ce:abstract']) == list:
-            abstract = data['doc:document']['ja:article']['ja:head']['ce:abstract'][0]['ce:abstract-sec']['ce:simple-para']['#text']
-            print(f"abstract: {abstract}")
-        elif type(data['doc:document']['ja:article']['ja:head']['ce:abstract']) == dict:
-            abstract = data['doc:document']['ja:article']['ja:head']['ce:abstract']['ce:abstract-sec']['ce:simple-para']['#text']
-            print(f"abstract: {abstract}")
+        abstract = ""
+        try:
+            if type(data['doc:document']['ja:article']['ja:head']['ce:abstract']) == list:
+                abstract = data['doc:document']['ja:article']['ja:head']['ce:abstract'][0]['ce:abstract-sec']['ce:simple-para']['#text']
+                print(f"abstract: {abstract}")
+            elif type(data['doc:document']['ja:article']['ja:head']['ce:abstract']) == dict:
+                abstract = data['doc:document']['ja:article']['ja:head']['ce:abstract']['ce:abstract-sec']['ce:simple-para']['#text']
+                print(f"abstract: {abstract}")
+        except:
+            continue
         introduction = ""
-        for i in data['doc:document']['ja:article']['ja:body']['ce:sections']['ce:section'][0]['ce:para']:
-            introduction = introduction + i.get('#text', '')
-        print(f"introduction: {introduction}")
+        try:
+            for i in data['doc:document']['ja:article']['ja:body']['ce:sections']['ce:section'][0]['ce:para']:
+                introduction = introduction + i.get('#text', '')
+            print(f"introduction: {introduction}")
+        except:
+            continue
         highlights = ""
         try:
             for i in data['doc:document']['ja:article']['ja:head']['ce:abstract'][2]['ce:abstract-sec']['ce:simple-para']['ce:list']['ce:list-item']:
@@ -43,11 +50,14 @@ for filename in os.listdir(directory):
             print(f"highlights: {highlights}")
 
         dictionary = {
-            "prompt": "Here is our : {introduction}\n\n###\n\n".format(introduction=introduction),
+            "prompt": "Here is our introduction: {introduction}\n\n###\n\n".format(introduction=introduction),
             "completion": "Here is our highlights: {highlights} END".format(highlights=highlights)
         }
         with open("sample.json", "a") as outfile:
-            json.dump(dictionary, outfile)
-            outfile.write("\n")
+            try:
+                json.dump(dictionary, outfile)
+                outfile.write("\n")
+            except:
+                continue
 
         mf.close()
